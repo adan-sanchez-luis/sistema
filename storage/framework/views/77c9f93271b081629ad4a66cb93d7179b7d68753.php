df9abd3607dd0f40bbd8106687eeb272e039e036
Omar Silverio Ibañez <17161247@itoaxaca.edu.mx>
2021-11-04 21:16:35 -0600

    cambio

---
+++ storage/framework/views/77c9f93271b081629ad4a66cb93d7179b7d68753.php
@@ -0,0 +1,186 @@
+<?php $__env->startSection('titulo', 'Permisos'); ?>
+<?php $__env->startSection('contenido'); ?>
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        
+        <?php echo $__env->make('plantilla.sidebar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <?php echo $__env->make('layouts.nav-log', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid rounded color">
+                    <br>
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 bold-title text-upper"> Listado de Permisos <i class="fas fa-user-cog"></i></h1>
+                    <p class="mb-4 text-dark">Consulte los datos de usuarios aquí.</p>
+
+
+                    
+                    <?php echo $__env->make('plantilla.notification', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4 rounded card-color">
+                        <div class="card-header py-3 bg-color">
+                            <h6 class="m-0 font-weight-bold">Búsqueda de usuarios por tipo y agregar permisos</h6>
+                        </div>
+
+
+                        <div class="card shadow  rounded card-color">
+                            <div class="container">
+                                <form action="<?php echo e(route('permission.index', [$permissions])); ?>" method="GET"> 
+                                                                    
+                                    <div class="row justify-content-md-center">
+                                        <div class="col-md-auto pt-3">
+                                            <div class="form-group">
+                                                <?php ($arrayB = [
+                                                    'NOMBRE',
+                                                    'FECHA'
+                                                    ]); ?>
+                                                    <select title="buscar por" class="form-control" name="type">
+                                                        <?php $__currentLoopData = $arrayB; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $buscar): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                                       
+                                                            <option><?php echo e($buscar); ?></option>
+                                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                                    </select>
+
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-auto pt-3">
+                                                <div class="form-group">
+                                                    <input class="form-control" name="buscarpor" type="search"
+                                                        placeholder="Buscar">
+
+                                                </div>
+                                            </div>
+
+
+                                            <div class="col-md-auto pt-3">
+                                                <div class="form-group">
+                                                    <button title="buscar permisos" class="btn btn-outline-primary text-black2"
+                                                        type="submit">Buscar</button>
+
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </form>
+                                </div>
+                                
+                            </div>
+
+                            <?php if($permissions->count()): ?>)
+                            <div class="card-body ">
+                                <div class="table-responsive">
+                                    
+                                    <table class="table  table-light" width="100%" cellspacing="0">
+                                        <thead class="bg-color ">
+                                            <tr class="text-blank text-center">
+                                                <th scope="col">ID</th>
+                                               
+                                                <th scope="col">NOMBRE</th>
+                                                <th scope="col">GUAR</th>
+                                                <th scope="col">FECHA</th>
+                                                <th colspan="2" scope="col">ACCIONES</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="text-black2">
+                                            <?php $__empty_1 = true; $__currentLoopData = $permissions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $permission): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
+                                                <tr class="table-hover">
+                                                    
+                                                    
+                                                    <td class="text-center"><?php echo e($permission->id); ?></td>
+                                                    <td class="text-center"><?php echo e($permission->name); ?></td>
+                                                    
+                                                    <td class="text-center"><?php echo e($permission->guard_name); ?></td>
+                                                    <td class="text-center"><?php echo e($permission->created_at); ?></td>
+                                                    
+                                                    <td class="text-center">
+                                                        <a title="editar permiso" href="<?php echo e(route('permission.edit', [$permission])); ?>"
+                                                            class="btn btn-outline-primary btn-circle">
+                                                            <i class="fa fa-edit"></i></a>
+
+                                                    </td>
+                                                    <td class="text-center">
+                                                        <form action="<?php echo e(route('permission.destroy', [$permission])); ?>" method="post"> 
+                                                            <?php echo csrf_field(); ?>
+                                                            <?php echo method_field("delete"); ?>
+                                                           
+                                                            <button title="borrar permiso" type="submit" class="btn btn-outline-danger btn-circle btn-delete">
+                                                                <i class="fa fa-trash"></i>
+                                                            </button>
+                                                        </form>
+                                                    </td>
+                                                </tr>
+                                                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); if ($__empty_1): ?>
+                                                <h3 class="text-black text-center"> ¡No hay registros!</h3>
+                                                <?php endif; ?>
+                                        </tbody>
+                                    </table>
+
+                                    <nav aria-label="Page navigation example float-right">
+                                        <a href="<?php echo e(route('permission.index')); ?>" class="btn btn-outline-primary mx-3 mt-3 " >refrescar</a>
+                                        <ul class="pagination float-right mt-3">
+                                            <li class="page-item"><a class="page-link"
+                                                    href="<?php echo e($permissions->previousPageUrl()); ?>">Anterior</a></li>
+                                            <li class="page-item"><a class="page-link" href="<?php echo e($permissions->url(1)); ?>">1</a>
+                                            </li>
+                                            <li class="page-item"><a class="page-link" href="<?php echo e($permissions->url(2)); ?>">2</a>
+                                            </li>
+                                            <li class="page-item"><a class="page-link" href="<?php echo e($permissions->url(3)); ?>">3</a>
+                                            </li>
+                                            <li class="page-item"><a class="page-link"
+                                                    href="<?php echo e($permissions->nextPageUrl()); ?>">Siguiente</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            <?php else: ?>
+                            <div class="card-body ">
+                               <div class=" row">
+                                <div class="col-md-4 mt-4">
+                                    <div class="form-group">
+                                        <a href="<?php echo e(route('permission.index')); ?>" class="btn btn-outline-primary" >regresar</a>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-md-8 mt-4">
+                                    <div class="form-group">
+                                        
+                                            <strong class ="card-title">¡No hay registros!</strong>
+                                       
+                                    </div>
+                                </div>
+                            </div>
+                               
+                                
+                                 </div>
+                             <?php endif; ?>
+                        </div>
+                        <br>
+                    </div>
+                    <!-- /.container-fluid -->
+
+                </div>
+                <!-- End of Main Content -->
+
+                <!-- Footer -->
+                <?php echo $__env->make('plantilla.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                <!-- End of Footer -->
+
+            </div>
+            <!-- End of Content Wrapper -->
+
+        </div>
+        <!-- End of Page Wrapper -->
+
+    <?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/sistema/resources/views/permissions/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/8ff09ff381468fed34f239a8ee674ed3eff478f0.php
@@ -0,0 +1,34 @@
+<div class="form-group">
+    <?php echo Form::label('nombre', 'Nombre del rol' ,['class'=>'text-black']); ?>
+
+ 
+     <?php echo Form::text('name', null, ['class'=>'form-control ',
+     'placeholder'=>'Ingrese el nombre del rol'
+     ]); ?>
+
+     <?php $__errorArgs = ['name'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+     <div class="message-error">*<?php echo e($message); ?></div>
+ <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>    
+</div>
+
+         <h3 class="h3 text-black2">Listado de roles</h3>
+         <div class="row justify-content-md-center">
+             <?php $__currentLoopData = $permissions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $permission): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+         
+             <div class="col col-lg-3">
+                 <label class="text-black2 h4">
+                    <?php echo Form::checkbox('permissions[]', $permission->id,null, ['class'=>'mr-1']); ?> 
+                     <?php echo e($permission->descripcion); ?>
+
+                 </label>
+             </div>
+             
+         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+     </div><?php /**PATH /Applications/XAMPP/xamppfiles/htdocs/sistema/resources/views/roles/partials/form.blade.php ENDPATH**/ ?>
